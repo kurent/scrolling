62f29f5e1a4101e84627433c95f01db77b958486
kurent <ales.kurent@gmail.com>
2018-10-17 09:35:59 +0200

    simple transform

---
+++ App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="body">
+        <vue-table></vue-table>
+    </div>
+</template>
+
+<script>
+import VueTable from './components/VueTable.vue'
+export default {
+    components: {
+        VueTable
+    }
+}
+</script>
+
+<style lang="css" scoped>
+body {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box; 
+}
+
+.body {
+    display: flex;
+}
+
+.header {
+    width: 100%;
+    background: black;
+}
+</style>
+
